f22096614c95fe963647500beb78de406db1c751
elev <elev@172.20.0.111>
2018-04-06 16:00:11 +0200

    Created framework for the pgm now only the matching of string is needed.

--- java1/src/java1/Program.java
+++ java1/src/java1/Program.java
@@ -1,10 +1,48 @@
 package java1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		System.out.println("Välj 1 för att räkna ord 2 för att räkna bokstäver");
+		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		if (Integer.parseInt(input.readLine()) == 1){
+			System.out.println("val1");
+			System.out.println(countWords(readInText()));
+			
+		}
+		else {
+			System.out.println("val2");
+			System.out.println(countLetters(readInText()));
+		}
+		
 
 	}
+	
+	public static int countWords(String string) {
+		//pattern och matcher
+		return 1;
+	}
+	public static int countLetters(String string) {
+		//pattern och matcher
+		return 1;
+	}
+	
+	public static String readInText() throws IOException {
+		
+		
+		
+		System.out.println("Skriv in textmassa avsluta med enter:");
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		return input.readLine();
+	}
+	
+	
 
 }
